abe8cab19260b0bd4bc1911130a3e41358432d5c
Theodore Chandra <Theodore Gautama Chandra>
2022-12-19 17:57:09 +0700

    read input as a string array

--- day3/main.go
+++ day3/main.go
@@ -4,21 +4,24 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
-	var content_string = readfile_to_string("input.txt")
-	fmt.Println(content_string)
+	var content_string = readfile_to_string_array("input.txt")
+	for _, string_value := range content_string {
+		fmt.Println(string_value)
+	}
 }
 
-func readfile_to_string(input_file string) string {
+func readfile_to_string_array(input_file string) []string {
 
 	content, err := os.ReadFile(input_file)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	content_str := string(content)
+	content_str := strings.Split(string(content), "\n")
 
 	return content_str
 }
