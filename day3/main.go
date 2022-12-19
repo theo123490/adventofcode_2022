b88252f2bf957ac05a93951b52d3bb8c3558192f
Theodore Chandra <Theodore Gautama Chandra>
2022-12-19 18:31:10 +0700

    get rucksack items

--- day3/main.go
+++ day3/main.go
@@ -10,13 +10,25 @@ import (
 func main() {
 	var content_string = readfile_to_string_array("input.txt")
 	var compartments []string
+	var rucksack_item int32
+	var items_map map[int32]int = make(map[int32]int)
 	for _, string_value := range content_string {
 		fmt.Println("----")
 		compartments = []string{
 			string_value[:len(string_value)/2],
 			string_value[len(string_value)/2:],
 		}
-		fmt.Println(compartments)
+
+		for _, value_compartment1 := range compartments[0] {
+			for _, value_compartment2 := range compartments[1] {
+				if value_compartment1 == value_compartment2 {
+					rucksack_item = value_compartment1
+				}
+			}
+		}
+
+		items_map[rucksack_item]++
+		fmt.Println(items_map)
 	}
 }
 
