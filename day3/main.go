93d862625c4da5042df6e4cb41ce479f46f951fe
Theodore Chandra <Theodore Gautama Chandra>
2022-12-19 19:29:36 +0700

    solve problem

--- day3/main.go
+++ day3/main.go
@@ -12,8 +12,11 @@ func main() {
 	var compartments []string
 	var rucksack_item int32
 	var items_map map[int32]int = make(map[int32]int)
+	var priority string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	var priority_sum int = 0
+	var item_value int
+
 	for _, string_value := range content_string {
-		fmt.Println("----")
 		compartments = []string{
 			string_value[:len(string_value)/2],
 			string_value[len(string_value)/2:],
@@ -21,15 +24,24 @@ func main() {
 
 		rucksack_item = get_rucksack_item(compartments)
 		items_map[rucksack_item]++
-		fmt.Println(items_map)
 	}
+
+	for item_key, item_amount := range items_map {
+		for item_value = 1; item_value <= 52; item_value++ {
+			if priority[item_value-1] == byte(item_key) {
+				priority_sum += int(item_amount) * int(item_value)
+			}
+		}
+	}
+	fmt.Println(items_map)
+	fmt.Println(priority_sum)
 }
 
 func get_rucksack_item(compartments []string) int32 {
 	for _, value_compartment1 := range compartments[0] {
 		for _, value_compartment2 := range compartments[1] {
 			if value_compartment1 == value_compartment2 {
-				return value_compartment1
+				return int32(value_compartment1)
 			}
 		}
 	}
