75d5e8a986fdec03c520221bb9810ad21bdf28f6
Theodore Chandra <Theodore Gautama Chandra>
2022-12-20 20:38:10 +0700

    add isFullyContainedIn function

--- day4/main.go
+++ day4/main.go
@@ -7,11 +7,21 @@ import (
 func main() {
 	ticketA := assignmentTicket{2, 4}
 	ticketB := assignmentTicket{1, 5}
-	fmt.Println(ticketA.StartLocation)
-	fmt.Println(ticketB.EndLocation)
+	ticketC := assignmentTicket{1, 3}
+
+	var AInB bool = ticketA.isFullyContainedIn(ticketB)
+	var AInC bool = ticketA.isFullyContainedIn(ticketC)
+	fmt.Printf("ticket a is fully contained in b = %v \n", AInB)
+	fmt.Printf("ticket a is fully contained in c = %v \n", AInC)
+	fmt.Printf("ticket b is fully contained in c = %v \n", ticketB.isFullyContainedIn(ticketC))
+	fmt.Printf("ticket b is fully contained in a = %v \n", ticketB.isFullyContainedIn(ticketA))
 }
 
 type assignmentTicket struct {
 	StartLocation int
 	EndLocation   int
+}
+
+func (ticket assignmentTicket) isFullyContainedIn(checkingTicket assignmentTicket) bool {
+	return (ticket.StartLocation > checkingTicket.StartLocation) && (ticket.EndLocation < checkingTicket.EndLocation)
 }
