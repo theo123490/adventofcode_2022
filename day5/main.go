b79152ea24f05e6154028812d6b5192c90bba8df
Theodore Chandra <Theodore Gautama Chandra>
2022-12-21 03:59:16 +0700

    add move stack function

--- day5/main.go
+++ day5/main.go
@@ -4,9 +4,23 @@ import "fmt"
 
 func main() {
 	stackA := stack{[]rune("abcd")}
-	fmt.Printf("%c", stackA.items[0])
+	stackB := stack{[]rune("xyz")}
+	stackA.moveItemTo(&stackB)
+	stackA.moveItemTo(&stackB)
+	for _, item := range stackA.items {
+		fmt.Printf("%c", item)
+	}
+	fmt.Println("\n----")
+	for _, item := range stackB.items {
+		fmt.Printf("%c", item)
+	}
 }
 
 type stack struct {
 	items []rune
+}
+
+func (currentStack *stack) moveItemTo(destinationStack *stack) {
+	destinationStack.items = append(destinationStack.items, currentStack.items[len(currentStack.items)-1])
+	currentStack.items = currentStack.items[:len(currentStack.items)-1]
 }
