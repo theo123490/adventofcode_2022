c9b9b41bcf5a2bd204510d9c8e7dba7fc2f8a999
Theodore Chandra <Theodore Gautama Chandra>
2022-12-20 21:35:14 +0700

    finish second question

--- day4/main.go
+++ day4/main.go
@@ -11,13 +11,17 @@ import (
 func main() {
 	var contentString []string = readFileToStringArray("input.txt")
 	var containedCounter int = 0
+	var overlappedCounter int = 0
 	for _, formatedMatchup := range contentString {
 		if isFormatMatchupEitherContained(formatedMatchup) {
 			containedCounter++
 		}
+		if isFormatMatchupOveralapped(formatedMatchup) {
+			overlappedCounter++
+		}
 	}
 	fmt.Println(containedCounter)
-
+	fmt.Println(overlappedCounter)
 }
 
 type assignmentTicket struct {
@@ -27,6 +31,15 @@ type assignmentTicket struct {
 
 func (ticket assignmentTicket) isFullyContainedIn(checkingTicket assignmentTicket) bool {
 	return (ticket.StartLocation >= checkingTicket.StartLocation) && (ticket.EndLocation <= checkingTicket.EndLocation)
+}
+
+func (ticket assignmentTicket) isOverlapped(checkingTicket assignmentTicket) bool {
+	var isStartIncluded bool = checkingTicket.StartLocation <= ticket.StartLocation && ticket.StartLocation <= checkingTicket.EndLocation
+	var isEndIncluded bool = checkingTicket.StartLocation <= ticket.EndLocation && ticket.EndLocation <= checkingTicket.EndLocation
+	var isCheckingStartIncluded bool = ticket.StartLocation <= checkingTicket.StartLocation && checkingTicket.StartLocation <= ticket.EndLocation
+	var isCheckingEndIncluded bool = ticket.StartLocation <= checkingTicket.EndLocation && checkingTicket.EndLocation <= ticket.EndLocation
+
+	return isStartIncluded || isEndIncluded || isCheckingEndIncluded || isCheckingStartIncluded
 }
 
 func readFileToStringArray(inputFile string) []string {
@@ -65,6 +78,17 @@ func isFormatMatchupEitherContained(matchupString string) bool {
 
 	isAinB := matchupTicketA.isFullyContainedIn(matchupTicketB)
 	isBinA := matchupTicketB.isFullyContainedIn(matchupTicketA)
+
+	return (isAinB || isBinA)
+}
+
+func isFormatMatchupOveralapped(matchupString string) bool {
+	var matchupStringArray []string = strings.Split(matchupString, ",")
+	matchupTicketA, _ := formatToTicket(matchupStringArray[0])
+	matchupTicketB, _ := formatToTicket(matchupStringArray[1])
+
+	isAinB := matchupTicketA.isOverlapped(matchupTicketB)
+	isBinA := matchupTicketB.isOverlapped(matchupTicketA)
 
 	return (isAinB || isBinA)
 }
