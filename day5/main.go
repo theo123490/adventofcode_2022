95d52b9c50f7bbad3e62edb9938432b7f2f0f779
Theodore Chandra <Theodore Gautama Chandra>
2022-12-21 15:59:28 +0700

    add a extractArray function

--- day5/main.go
+++ day5/main.go
@@ -8,24 +8,16 @@ import (
 
 func main() {
 	inputString := fileManipulation.ReadFileToStringArray("input.txt")
-	for _, value := range inputString {
-		fmt.Println(value)
-	}
 	inputString = fileManipulation.RotateArrayClockwise(inputString)
+	inputString = extractArray(inputString)
 	for _, value := range inputString {
 		fmt.Println(value)
 	}
+
 	stackA := stack{[]rune("abcd")}
 	stackB := stack{[]rune("xyz")}
 	stackA.moveItemTo(&stackB)
 	stackA.moveItemTo(&stackB)
-	for _, item := range stackA.items {
-		fmt.Printf("%c", item)
-	}
-	fmt.Println("\n----")
-	for _, item := range stackB.items {
-		fmt.Printf("%c", item)
-	}
 }
 
 type stack struct {
@@ -35,4 +27,15 @@ type stack struct {
 func (currentStack *stack) moveItemTo(destinationStack *stack) {
 	destinationStack.items = append(destinationStack.items, currentStack.items[len(currentStack.items)-1])
 	currentStack.items = currentStack.items[:len(currentStack.items)-1]
+}
+
+func extractArray(stringArray []string) []string {
+	var newStringArray []string = make([]string, 0)
+	for _, stringLine := range stringArray {
+		if stringLine[0] == 32 {
+			continue
+		}
+		newStringArray = append(newStringArray, stringLine[1:])
+	}
+	return newStringArray
 }
